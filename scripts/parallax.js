edd5b81e3e89c75dce6312eeaa810d978aedad9c
NicolayKjarnet <nicolaysmedier@gmail.com>
2024-07-21 10:20:57 +0200

    Update CSS and JS for better user experience and bug fixes.

--- scripts/parallax.js
+++ scripts/parallax.js
@@ -3,6 +3,25 @@ gsap.registerPlugin(ScrollTrigger);
 gsap.utils.toArray(".image-container").forEach((container) => {
   const imgs = container.querySelectorAll("img");
 
+  // Block parallax effect on mobile for better user experience
+  const screenWidth = window.innerWidth;
+
+  if (screenWidth < 768) {
+    imgs.forEach((img) => {
+      gsap.to(img, {
+        yPercent: 0,
+        ease: "none",
+        scrollTrigger: {
+          trigger: container,
+          start: "top bottom",
+          end: "bottom top",
+          scrub: true,
+        },
+      });
+    });
+    return;
+  }
+
   imgs.forEach((img) => {
     gsap.to(img, {
       yPercent: -40,
